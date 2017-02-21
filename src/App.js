f3c8789da943b155087b3d42547d09884b0f1398
Nick Riddell <nickjriddell@gmail.com>
2017-02-21 12:20:46 +0000

    practice todo with tests

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {TodoForm, TodoList} from './components/todo'
+import {addTodo, generateId, findById, toggleTodo, updateTodo, removeTodo} from './lib/todoHelpers'
+import {pipe, partial} from './lib/utils'
+
+class App extends Component {
+  state = {
+    todos: [
+      {id: 1, name: 'Learn React', isComplete: true},
+      {id: 2, name: 'Built', isComplete: false},
+      {id: 3, name: 'Ship', isComplete: false},
+      {id: 4, name: '...', isComplete: false},
+      {id: 5, name: 'Cash Money', isComplete: false}
+    ],
+    currentTodo: ''
+  }
+
+  handleRemove = (id, evt) => {
+    evt.preventDefault()
+    const updatedTodos = removeTodo(this.state.todos, id)
+    this.setState({todos: updatedTodos})
+  }
+
+  handleToggle = (id) => {
+    const getUpdatedTodos = pipe(findById, toggleTodo, partial(updateTodo, this.state.todos))
+    const updatedTodos = getUpdatedTodos(id, this.state.todos)
+    this.setState({todos: updatedTodos})
+  }
+
+  handleSubmit = (evt) => {
+    evt.preventDefault()
+    const newId = generateId()
+    const newTodo = {id: newId, name: this.state.currentTodo, isComplete: false}
+    const updatedTodos = addTodo(this.state.todos, newTodo)
+    this.setState({
+      todos: updatedTodos,
+      currentTodo: '',
+      errorMessage: ''
+    })
+  }
+
+  handleEmptySubmit = (evt) => {
+    evt.preventDefault()
+    this.setState({
+      errorMessage: 'Please supply a todo name'
+    })
+  }
+
+  handleInputChange = (evt) => {
+    this.setState({
+      currentTodo: evt.target.value
+    })
+  }
+
+  render() {
+    const submitHandler = this.state.currentTodo ? this.handleSubmit : this.handleEmptySubmit
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>React Todos</h2>
+        </div>
+        <div className="Todo-App">
+          {this.state.errorMessage && <span className='error'>{this.state.errorMessage}</span>}
+          <TodoForm handleInputChange={this.handleInputChange}
+            currentTodo={this.state.currentTodo}
+            handleSubmit={submitHandler}/>
+            <TodoList handleToggle={this.handleToggle} 
+              todos={this.state.todos}
+              handleRemove={this.handleRemove} />          
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
